218c7df656844aecec342c68b2fc754fb2a34dc1
Michal Simek <michal.simek@xilinx.com>
2013-10-31 11:22:07 +0100

    Append information about arasan sdhci version
    Just follow proposed binding for the mainline kernel.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- data/device-tree.tcl
+++ data/device-tree.tcl
@@ -1833,7 +1833,7 @@ proc gener_slave {node slave_ip intc {force_type ""} {busif_handle ""}} {
 			lappend node $ip_tree
 		}
 		"ps7_sdio" {
-			set ip_tree [slaveip $slave $intc "" [default_parameters $slave] "S_AXI_" "generic-sdhci arasan,sdhci"]
+			set ip_tree [slaveip $slave $intc "" [default_parameters $slave] "S_AXI_" "generic-sdhci arasan,sdhci-8.9a"]
 			# FIXME linux sdhci requires clock-frequency even if we use common clock framework
 			set ip_tree [tree_append $ip_tree [list "clock-frequency" int [get_ip_param_value $slave "C_SDIO_CLK_FREQ_HZ"]]]
 			set ip_tree [zynq_irq $ip_tree $intc $name]
