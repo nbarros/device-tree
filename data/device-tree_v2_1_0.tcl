a49ad1f5717c30de8e56c3261e4abb32b497f1c7
Michal Simek <michal.simek@xilinx.com>
2014-02-06 09:31:20 +0100

    Fix i2c compat string generation
    xlnx,zynq-i2c-1.00.a has never been used in our tree
    just for developing. Use cadence compat string instead.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- data/device-tree_v2_1_0.tcl
+++ data/device-tree_v2_1_0.tcl
@@ -2152,7 +2152,7 @@ proc gener_slave {node slave intc {force_type ""} {busif_handle ""}} {
 			set alias_node [list i2c$i2c_count aliasref $name $i2c_count]
 			lappend alias_node_list $alias_node
 
-			set ip_tree [slaveip $slave $intc "" [default_parameters $slave "C_I2C_RESET"] "S_AXI_" "xlnx,zynq-i2c-1.00.a"]
+			set ip_tree [slaveip $slave $intc "" [default_parameters $slave "C_I2C_RESET"] "S_AXI_" "cdns,i2c-r1p10"]
 			# use TCL table
 			set ip_tree [zynq_irq $ip_tree $intc $name]
 			set ip_tree [zynq_clk $ip_tree $name]
