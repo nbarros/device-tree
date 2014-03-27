962645e7c14540f4bd29f84e9627aeb29422424a
Michal Simek <michal.simek@xilinx.com>
2014-03-27 15:09:17 +0100

    zynq: Change compatible string for ddrc
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- data/device-tree_v2_1_0.tcl
+++ data/device-tree_v2_1_0.tcl
@@ -2109,7 +2109,9 @@ proc gener_slave {node slave intc {force_type ""} {busif_handle ""}} {
 			lappend node $ip_tree
 		}
 		"ps7_ddrc" {
-			set ip_tree [slaveip $slave $intc "" [default_parameters $slave] "S_AXI_" "xlnx,zynq-ddrc-1.00.a"]
+			set ip_tree [slaveip $slave $intc "" [default_parameters $slave] "S_AXI_"]
+			set ip_tree [tree_node_update $ip_tree "compatible" [list "compatible" stringtuple "xlnx,zynq-ddrc-1.0"]]
+
 			# use TCL table
 			set ip_tree [zynq_irq $ip_tree $intc $name]
 			set ip_tree [zynq_clk $ip_tree $name]
